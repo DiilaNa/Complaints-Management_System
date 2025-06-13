296bf903a32c2a8c7b5402c1421b94ad34d8f509
DiilaNa <liyanaarachchidilan@gmail.com>
2025-06-13 18:05:42 +0530

    Add User Servlet doPost Method

--- src/main/java/lk/ijse/project/controller/UserServlet.java
+++ src/main/java/lk/ijse/project/controller/UserServlet.java
@@ -15,6 +15,24 @@ public class UserServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
     }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+        String role = req.getParameter("role");
 
+        User user = new User(Integer.parseInt(id), name, email, password, role);
+
+        lk.ijse.project.model.dao.UserDAO userDAO = new lk.ijse.project.model.dao.UserDAO(getServletContext());
+        boolean success = userDAO.createUser(user);
+
+        if (success) {
+            resp.sendRedirect("login.jsp");
+        } else {
+            resp.sendRedirect("register.jsp?error=true");
+        }
+    }
 
 }
