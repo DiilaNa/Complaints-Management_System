f0ef71dd0619c112ef1b57004dac923f83030ecc
DiilaNa <liyanaarachchidilan@gmail.com>
2025-06-13 17:42:18 +0530

    add DBCP Conection

---
+++ src/main/java/lk/ijse/project/model/Db/Db.java
@@ -0,0 +1,40 @@
+package lk.ijse.project.model.Db;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.SQLException;
+
+@WebListener
+public class Db implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        BasicDataSource ds = new BasicDataSource();
+        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/complaintsMngSystem");
+        ds.setUsername("root");
+        ds.setPassword("Ijse@1234");
+        ds.setInitialSize(50);
+        ds.setMaxTotal(100);
+
+        ServletContext sc = sce.getServletContext();
+        sc.setAttribute("ds", ds);
+
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        try {
+            ServletContext sc = sce.getServletContext();
+            BasicDataSource ds = (BasicDataSource) sc.getAttribute("ds");
+            if (ds != null) {
+                ds.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
