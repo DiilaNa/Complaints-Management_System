97f05c33f110af87a1a47fb962291bcb8f164887
DiilaNa <liyanaarachchidilan@gmail.com>
2025-06-13 17:47:22 +0530

    Add user Bean Class

--- src/main/java/lk/ijse/project/model/Entity/User.java
+++ src/main/java/lk/ijse/project/model/Entity/User.java
@@ -1,4 +1,16 @@
 package lk.ijse.project.model.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
 }
